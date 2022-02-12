2265dc1ad171ebdbe6be7d0714ed299c9d096087
mdricard <ricard@uta.edu>
2022-02-12 14:53:15 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+x = [2.2, 3.3, 4.4, 5.5]
+
+def print_list():
+    # Use a breakpoint in the code line below to debug your script.
+    print(x)  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_list()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
